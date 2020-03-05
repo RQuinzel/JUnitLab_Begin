11bdb6e1ea79d8dd87b6d5c12f588e1e4f027938
Rosie Quinzel <rquinzel21@gmail.com>
2020-03-05 14:20:16 -0800

    More tests! Eclemma!

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -20,5 +20,36 @@ public class GiftCardTest
         
         assertEquals("getIssuingStore()", issuingStore, card.getIssuingStore());
     }
+    
+    @Test
+    public void getBalance()
+    {
+        double balance;
+        GiftCard card;
+        int issuingStore;
+        
+        issuingStore = 1337;
+        balance = 100.00;
+        card = new GiftCard(issuingStore, balance);
+        
+        assertEquals("getBalance()", balance, card.getBalance(), 0.001);
+    }
 
+    @Test
+    public void deduct()
+    {
+        double balance;
+        GiftCard card;
+        int issuingStore;
+        
+        issuingStore = 1337;
+        balance = 100.00;
+        card = new GiftCard(issuingStore, balance);
+        
+        assertEquals("deduct()", "Remaining Balance: " + String.format("%6.2f", Math.abs(75.00)), card.deduct(25.00));
+        
+        assertEquals("deduct()", "Invalid Transaction", card.deduct(-1.0));
+        
+        assertEquals("deduct()", "Amount Due: " + String.format("%6.2f", Math.abs(-25.00)), card.deduct(100.00));
+    }
 }
